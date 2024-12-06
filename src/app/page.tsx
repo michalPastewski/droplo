03193988f8e87c4068a9eb4dab822582355fb677
MPastewski <michpastewski@gmail.com>
2024-12-06 12:39:55 +0100

    DROPLO: refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,16 @@ export default function Home() {
       <main className='flex_center flex-col py-12 px-12 gap-4 text-lg'>
         HELLO DROPLO
         <section>
-          <p>Cześć, jeśli tu jesteś to świetnie. Któś przynajmniej zerknie na ten cod a to ju coś.</p>
+          <p>Cześć,</p>
+          <p>jeśli tu jesteś to świetnie. Któś zerknie na ten kod a to juz coś.</p>
           <p>Cieszę się ze mogłem uczestniczyć w Waszej rekrutacji, przysporzyło mi to duzo frajdy.</p>
-          <p>Pisanie kodu od zera to zawsze faina zabawa, a jeśli w DROPLO moecie robić to często to wielkie zazdro :D</p>
-          <p>Niestety miałem cięki tydzień i nie mogłem przeznaczyć na projekt tyle czasu ile bym chciał...
-            Dlatego nie wszytko zostało zrealizowane :()
+          <p>Pisanie kodu od zera to zawsze fajna zabawa, a jeśli w DROPLO mozecie robić to często to wielkie zazdro :D</p>
+          <p>Niestety miałem cizęki tydzień i nie mogłem przeznaczyć na projekt tyle czasu ile bym chciał...
+            Dlatego nie wszytko zostało zrealizowane :(
           </p>
-          <p>Niemniej będzie mi miło jeśli zerkniesz na kod i stwierdzisz ze jest całkiem spoko (oby)</p>
-          <p>Było mi bardzo miło! Być moze do zobaczenia na kolejnej rekrutacji. Liczę na jakieś fajne zadanko :D</p>
+          <p>Niemniej będzie mi miło jeśli zerkniesz na kod i stwierdzisz ze jest całkiem spoko (oby).</p>
+          <p>Było mi bardzo miło! </p>
+          <p>Być moze do zobaczenia na kolejnej rekrutacji. Liczę na jakieś fajne zadanko :D</p>
         </section>
         <Link href="/navigations" className='border_primary bg-blue-100 py-4 px-5 w-1/2 text-center text-blue-800'>Przejdź do zadania</Link>
       </main>
